e58666fe1d8c1c8288daf1144de44f89e6a45188
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-27 16:27:38 -0500

    Parametros nombrados y opcionales

--- P21Streams/Program.cs
+++ P21Streams/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace P21Streams
 {
@@ -6,7 +7,31 @@ namespace P21Streams
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long cantidad = 0;//los streams trabajan con longs
+            long n = 0;
+            int valor = 0;
+
+            FileStream fs = new FileStream("datos.txt", FileMode.Open, FileAccess.Read, FileShare.None);
+
+            //obtenemos la cantidad
+            cantidad = fs.Length;
+            Console.WriteLine($"El archivo mide {cantidad}");
+
+            //Leemos byte por byte
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                //ponemos la posicion, este es el offset
+                fs.Seek(n, SeekOrigin.Begin);
+
+                valor = fs.ReadByte();
+                Console.WriteLine("{0}", (char)valor);
+            }
+
+            Console.WriteLine("\n--------\n");
+
+            fs.Close();
+
         }
     }
 }
---
+++ P22ParametrosNombrados-Opcionales/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace P22ParametrosNombrados_Opcionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Invocamos la funcion de forma normal
+            //tres valores posicionales
+            mostrarDatos(3, 5, 7);
+
+            /*
+             * Invocamos la funcion con primero en normal, 
+             * los demas en otro orden, 
+             */
+            mostrarDatos(2, pC: 11, pB: 12);
+
+            //Un posicional no puede venir despues de un nombrado
+            //mostrarDatos(pB: 5, 7, 8);
+
+            //Dos opcionales con uno nombrado
+            mostrarOpcionales(pC: 12);
+
+        }
+
+        public static void mostrarDatos(int pA, int pB, int pC)
+        {
+            Console.WriteLine("El valor de A es {0}", pA);
+            Console.WriteLine("El valor de B es {0}", pB);
+            Console.WriteLine("El valor de C es {0}", pC);
+            Console.WriteLine("----------------------");
+        }
+
+        //Si damos por opcional a uno debemos hacerlo en todos
+        //los parametros
+        public static void mostrarOpcionales(int pA = 1, 
+            int pB = 2, int pC = 3)
+        {
+            Console.WriteLine("El valor de A es {0}", pA);
+            Console.WriteLine("El valor de B es {0}", pB);
+            Console.WriteLine("El valor de C es {0}", pC);
+            Console.WriteLine("----------------------");
+        }
+    }
+}
