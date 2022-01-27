945b0bc9d6bbe3252ad2a54f9608056abb0b82a6
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-27 09:07:41 -0500

    Sobrecarga de operadores binarios
    Se pueden usar los operadores binarios para operar con propiedades de objetos, pero toca hacer un método especial para ello.

---
+++ SobrecargaOperadoresBinarios/CImaginario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaOperadoresBinarios
+{
+    internal class CImaginario
+    {
+        private double entero;
+        private double imaginario;
+
+        public CImaginario(double pEntero, double pImaginario)
+        {
+            entero = pEntero;
+            imaginario = pImaginario;
+        }
+
+        //propiedades
+        public double Entero { get { return entero; } set { entero = value; } }
+        public double Imaginario { get { return imaginario; } set { imaginario = value; } }
+
+        public override string ToString()
+        {
+            if (imaginario < 0)
+                return string.Format("({0} {1}i)", entero, imaginario);
+            return
+                string.Format("({0} + {1}i)", entero, imaginario);
+        }
+        //Sobrecarga de los operadores binarios
+        public static CImaginario operator + (CImaginario i1, CImaginario i2)
+        {
+            double re = 0;
+            double ri = 0;
+
+            re = i1.Entero + i2.Entero;
+            ri = i1.Imaginario + i2.Imaginario;
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+    }
+}
---
+++ SobrecargaOperadoresBinarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SobrecargaOperadoresBinarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CImaginario im1 = new CImaginario(1, 2);
+            CImaginario im2 = new CImaginario(3, 4);
+
+            CImaginario imr;
+
+            //Hacemos la suma
+            imr = im1 + im2;
+
+            Console.WriteLine($"{im1} + {im2} = {imr}");
+        }
+    }
+}
