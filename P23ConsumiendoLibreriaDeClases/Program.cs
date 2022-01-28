a35f830bf6197e74a31eb9145b7c828dd6c40b75
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-28 09:01:36 -0500

    Haciendo biblioteca reusable

---
+++ P23ConsumiendoLibreriaDeClases/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using P23HaciendoLibreriaDeClases;
+
+namespace P23ConsumiendoLibreriaDeClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( CMates.Suma( 9, 8 ) );
+        }
+    }
+}
---
+++ P23HaciendoLibreriaDeClases/CMates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P23HaciendoLibreriaDeClases
+{
+    public class CMates
+    {
+        public static double Suma(double a, double b) => a + b;
+        public static double Resta(double a, double b) => a - b;
+        public static double Multiplicacion(double a, double b) => a * b;
+        public static double Division(double a, double b) => a / b;
+    }
+}
