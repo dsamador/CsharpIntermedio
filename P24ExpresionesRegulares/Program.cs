64556c413c9b2466046a62eb996ccf93055f9e86
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-31 18:13:08 -0500

    programa para ayudarme con informes

---
+++ P24ExpresionesRegulares/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P24ExpresionesRegulares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metodo 1
+
+            string texto = "Javascript no es Java ni se escribe Javahiana";
+            string exp = "Java";
+
+            MatchCollection encontrado = Regex.Matches(texto, exp);
+
+            // Va a mostrar Java tres veces
+            foreach (Match match in encontrado) Console.WriteLine(match);
+
+            Console.WriteLine("---------");
+
+            string exp2 = " Java ";
+
+            Regex regex = new Regex(exp2);
+
+            encontrado = regex.Matches(texto);
+
+            //Muestra Java una sola vez
+            foreach (var item in encontrado) Console.WriteLine($"Un solo {item}"); ;
+
+
+        }
+    }
+}
---
+++ dividirpor12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dividirpor12
+{//Programa para ayudarme a sacar promedios un informe
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Double> list = new List<Double>();
+            while (true)
+            {
+                int cantidadValores = 1;
+                Console.WriteLine("Nueva cantidad de valores");
+                while(cantidadValores < 6)
+                {
+                    Console.WriteLine($"Valor numero {cantidadValores}");
+                    double valor = Double.Parse(Console.ReadLine());
+
+                    if (valor == 0)
+                        valor = 1;
+
+                    double promedio = valor / 12;
+                    
+                    list.Add(promedio);
+                    
+                    cantidadValores++;
+                }
+                Console.WriteLine("-----------");
+                Console.WriteLine("--Valores--");
+                int indice = 1;
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"Valor {indice} {item}");
+                    indice++;
+                }
+                Console.WriteLine("\n");
+                list.Clear();
+            }
+        }
+    }
+}
