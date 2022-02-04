8bea5ddc5f1ed4d24a59635b0c7ff725a35103db
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-04 11:16:54 -0500

    Burbuja terminada

---
+++ Estructuras10BubbleSort/CNodo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras10BubbleSort
+{
+    internal class CNodo
+    {
+        //Aqui colocamos el dato o los datos que guarda el nodo        
+        private int dato;
+
+        //Esta variable de referencia es usada para apuntar al nodo siguiente
+        //una variable de referencia es una variable que puede ser referenciada
+        //a cualquier instancia del mismo tipo de la clase. Por seguridad se
+        //inicia en null
+        private CNodo siguiente = null;
+
+        //propiedades que usaremos
+        public int Dato { get => dato; set => dato = value; }
+        internal CNodo Siguiente { get => siguiente; set => siguiente = value; }
+
+        //Para su facil impresion
+        public override string ToString()
+        {
+            return string.Format("[{0}]", dato);
+        }
+    }
+}
--- Estructuras10BubbleSort/Program.cs
+++ Estructuras10BubbleSort/Program.cs
@@ -4,9 +4,45 @@ namespace Estructuras10BubbleSort
 {
     internal class Program
     {
+        private static CListaLigada miLista = new CListaLigada();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            miLista.Adicionar(3);
+            miLista.Adicionar(2);
+            miLista.Adicionar(37);
+            miLista.Adicionar(300);
+            miLista.Adicionar(8);
+            miLista.Adicionar(65);
+            miLista.Adicionar(45);
+            miLista.Adicionar(76);
+            miLista.Adicionar(12);
+            miLista.Adicionar(27);
+
+            miLista.Transversa();
+
+            int m = 0;
+            int n = 0;
+            int cantidad = miLista.Cantidad();
+
+            Console.WriteLine($"Son {cantidad} elementos");
+
+            //Hacemos los pases correspondientes
+            for(m = 1; m < cantidad; m++)
+            {
+                for (n = 0; n < cantidad - m; n++)
+                {
+                    if (miLista[n] > miLista[n + 1])
+                        Swap(n, n + 1);
+                }
+            }
+            miLista.Transversa();
+        }
+
+        private static void Swap(int i1, int i2)
+        {
+            int temp = miLista[i1];
+            miLista[i1] = miLista[i2];
+            miLista[i2] = temp;
         }
     }
 }
