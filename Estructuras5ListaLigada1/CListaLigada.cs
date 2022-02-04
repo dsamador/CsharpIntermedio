177db15fb72733dfe10c35527b6edb7829e08f6b
David Amador <54488581+dsamador@users.noreply.github.com>
2022-02-04 10:35:38 -0500

    Operaciones básicas de lista ligada

--- Estructuras5ListaLigada1/CListaLigada.cs
+++ Estructuras5ListaLigada1/CListaLigada.cs
@@ -113,5 +113,126 @@ namespace Estructuras5ListaLigada1
             //no se encontro, regresamos null
             return null;
         }
+
+        //Busca el indice donde se encuentra la primera ocurrencia del dato
+        public int Buscarindice(int pDato)
+        {
+            int n = -1;
+
+            trabajo = ancla;
+            while(trabajo.Siguiente != null)
+            {
+                trabajo = trabajo.Siguiente;
+                n++;
+
+                if (trabajo.Dato == pDato)
+                    return n;
+            }
+            return -1;
+        }
+
+        //encuentra al nodo anterior
+        //si esta en el primer nodo se regresa el ancla
+        //si el dato no existe se regresa el ultimo nodo
+        public CNodo BuscarAnterior(int pDato)
+        {
+            trabajo2 = ancla;
+            while (trabajo2.Siguiente != null && trabajo2.Siguiente.Dato != pDato)
+                trabajo2 = trabajo2.Siguiente;
+            return trabajo2;
+        }
+
+        //borra la primera ocurrencia del dato
+        public void Borrar(int pDato)
+        {
+            //verificamos que se tengan datos
+            if (EstaVacio() == true)
+                return;
+            
+            //buscamos los nodos con los que trabajamos
+            CNodo anterior = BuscarAnterior(pDato);
+
+            CNodo encontrado = Buscar(pDato);
+
+            //si no hay nodo a borrar salimos
+            if (encontrado == null)
+                return;
+
+            //brincamos el nodo
+            anterior.Siguiente = encontrado.Siguiente;
+
+            //quitamos el actual de la lista
+            encontrado.Siguiente = null;
+        }
+
+        //inserta el dato pValor despues de la primera ocurrencia del dato pasado pDonde
+        public void Insertar(int pDonde, int pValor)
+        {
+            //encontramos la posicion donde vamos a insertar
+            trabajo = Buscar(pDonde);
+
+            //verificamos que exista donde vamos a insertar
+            if(trabajo == null)
+                return;
+
+            //creamos el nodo temporal a insertar
+            CNodo temp = new CNodo();
+            temp.Dato = pValor;
+
+            //conectamos el tempora a la lista
+            temp.Siguiente = trabajo.Siguiente;
+
+            //conectamos trabajo a temporal
+            trabajo.Siguiente = temp;
+        }
+
+        public void InsertarInicio(int pDato)
+        {
+            //creamos el nodo temporal
+            CNodo temp = new CNodo();
+            temp.Dato = pDato;
+
+            //conectamos el temporal a la lista
+            temp.Siguiente = ancla.Siguiente;
+
+            //conectamos el ancla al temporal
+            ancla.Siguiente = temp;
+        }
+
+        //obtenemos la referencia al nodo dado el indice
+        public CNodo ObtenerPorIndice(int pIndice)
+        {
+            CNodo trabajo2 = null;
+            int indice = -1;
+
+            trabajo = ancla;
+
+            while(trabajo.Siguiente != null)
+            {
+                trabajo = trabajo.Siguiente;
+                indice++;
+
+                if (indice == pIndice)
+                    trabajo2 = trabajo;
+            }
+            return trabajo2;
+        }
+
+        //indexer, con esto podemos usar sintaxis de arreglo
+        public int this[int pIndice]
+        {
+            get
+            {
+                trabajo = ObtenerPorIndice(pIndice);
+                return trabajo.Dato;
+            }
+
+            set
+            {
+                trabajo = ObtenerPorIndice(pIndice);
+                if(trabajo != null)
+                    trabajo.Dato = value;
+            }
+        }
     }
 }
